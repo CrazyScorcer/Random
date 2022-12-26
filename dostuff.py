dd7e61c8e2138f4d1da584fc1dfc1391b932a4db
Eric <ericmorse121@gmail.com>
2022-12-26 10:26:55 -0700

    added insert feature

--- dostuff.py
+++ dostuff.py
@@ -20,41 +20,51 @@ while True:
                     except ValueError:
                         print("Invalid Input. Try Again.")
                 # Contructs command string that will be execute to create table with columns
-                columnCommand = ""
+                columnCommandList = []
                 for x in range(columnAmount):
                     currentColumnNumber = x+1
-                    columnCreate = input(f"Input column {currentColumnNumber} name and type:")
-                    if x == 0:
-                        columnCommand += columnCreate
-                    else:
-                        columnCommand += f", {columnCreate}"
-                tableCreateCommand = f"CREATE TABLE {tableName} ({columnCommand})"
-                print(tableCreateCommand)
-                cursor.execute(tableCreateCommand)
+                    columnCreate = input(f"Input Column {currentColumnNumber} Name and Type:")
+                    columnCommandList.append(columnCreate)
+                columnCommand = ", ".join(columnCommandList)
+                cursor.execute(f"CREATE TABLE {tableName} ({columnCommand})")
                 connection.commit()
-                print("Successfully created table")
-            except:
-                print("Failed to create table")
+                print("Successfully Created Table")
+            except Exception as e:
+                print(e)
+                print("Failed To Create Table")
         case("DELETE TABLE"):
             try:
-                tableName = input("Input table name: ")
+                tableName = input("Input Table Name: ")
                 cursor.execute(f"DROP TABLE {tableName}")
-                print("Succesfully deleted table")
-            except:
-                print("Faild to delete table")
+                connection.commit()
+                print("Succesfully Deleted Table")
+            except Exception as e:
+                print(e)
+                print("Failed To Delete Table")
         case("INSERT"):
-            #try:
-                tableName = input("Input table name: ")
+            try:
+                tableName = input("Input Table Name: ")
                 pragma = cursor.execute(f"PRAGMA table_info({tableName})").fetchall()
-                columnNum = 1
+                columnNum = 0
                 for column in pragma:
-                    print(f"Column {columnNum}: {list(column)[1]} {list(column)[2]}")
                     columnNum += 1
-                
-            #except:
-                print("Failed to ")
+                    print(f"Column {columnNum}: {list(column)[1]} {list(column)[2]}")
+                columnValues = []
+                sanitizerList = []
+                for x in range(columnNum):
+                    currentColumnNumber = x+1
+                    #columninput = input(f"Input Column {currentColumnNumber} Value: ")
+                    columnValues.append(input(f"Input Column {currentColumnNumber} Value: "))
+                    sanitizerList.append("?")
+                sanitizer = ",".join(sanitizerList)
+                sanitizerTuple = sanitizer
+                cursor.execute(f"INSERT INTO {tableName} VALUES({sanitizerTuple})",(tuple(columnValues)))
+                connection.commit()
+            except Exception as e:
+                print(e)
+                print("Failed to Insert Element")
         case _:
             print("Invalid Command")
 
-connection.commit()
+
 connection.close()
